755d20433214a39807e5862be3e6604a592939f7
ogbonnacc <cogbonna@aims.edu.ng>
2014-08-30 12:41:27 +0000

    the message

--- Esteem.py
+++ Esteem.py
@@ -1,4 +1,7 @@
 from numbers import Number
+import numpy as np
+
+
 def square_perimeter(side : Number) -> Number:
     """
     calculate perimeter of a square from side length.
@@ -8,6 +11,7 @@ def square_perimeter(side : Number) -> Number:
     16
     """
     return 4*side
+
 
 def square_area(side):
     """
@@ -19,6 +23,28 @@ def square_area(side):
     """
     return side*side
 
+
+def segment_area(radius, breadth, height, theta1, theta2):
+    """
+
+    :param radius: length of one side of sector
+    :param breadth: base of triangle
+    :param height: height of triangle
+    :param theta1: angle of sector
+    :param theta2: angle of triangle
+    :return:segment area in units^2
+    segment_area(10, 12, 8, np.pi/4, np.pi/4)
+    5.239
+    """
+
+    return ((theta1/360)*np.pi*radius**2) - (0.5*breadth*height*np.sin(theta2))
+
 if __name__ == "__main__":
+    sampleRadius = 12
+    sampleBreadth = 10
+    sampleHeight = 14
+    sampleTheta1 = np.pi/4
+    sampleTheta2 = np.pi/4
     sampleSide = 12
-    print("area:", square_area(sampleSide),"perimeter:",square_perimeter(sampleSide))
+    print("area:", square_area(sampleSide),"perimeter:",square_perimeter(sampleSide), "area:",
+          segment_area(sampleRadius,sampleBreadth, sampleHeight, sampleTheta1, sampleTheta2,))
